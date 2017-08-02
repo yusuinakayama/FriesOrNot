536e1d704e4b6289edea51cedf52cd59726622ab
中山湧水 <yusui@nakayamayuusuinoMacBook-Pro.local>
2017-08-02 13:43:51 +0900

    Initial Commit

---
+++ FriesOrNot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriesOrNot
+//
+//  Created by 中山湧水 on 2017/08/02.
+//  Copyright © 2017年 中山湧水. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
